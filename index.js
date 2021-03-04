9f7eb47eda38837964d156b0e59f84a3859bc914
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-03-04 15:32:59 +0200

    Update the files with http methods

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const path = require("path");
+const logger = require("./middleware/logger");
 
 const app = express();
 
 // Set Static Folder
 // app.use(express.static(path.join(__dirname, "public")));
+
+// Init middleware
+// app.use(logger);
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Members API Routes
+app.use("/api/members", require("./routes/api/members"));
 
 const PORT = process.env.PORT || 5000;
 
